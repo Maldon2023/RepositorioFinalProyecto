d6ead51142fe94555ed7a83bafa2886107a2bc32
Mariano Donamari <marianodonamari8@gmail.com>
2023-09-19 19:08:44 -0300

    Commit con clase conexion 19/09
    Primera clase armada (conexion)

--- src/AccesoADatos/Conexion.java
+++ src/AccesoADatos/Conexion.java
@@ -5,10 +5,41 @@
  */
 package AccesoADatos;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 /**
- *
- * @author Alumno
+ 
  */
 public class Conexion {
+    private static final String URL = "jdbc:mariadb://localhost/" ;
+    private static final String DB = "universidadUlp_equipo27";
+    private static final String USUARIO = "root";
+    private static String PASSWORD= "";
+    private static Connection connection;
+
+    public Conexion() {
+    }
+    public static Connection Revisarconexion(){
+       if  (connection == null){
+           try{
+               Class.forName("org.mariadb.jdbc.Driver");
+               connection = DriverManager.getConnection (URL + DB+"?useLegacyDatetimeCode = false&severTimezone = UTC " + "&user= "  + USUARIO + " &password = " + PASSWORD );
+       
+           } catch ( SQLException    uni  ){
+               JOptionPane.showMessageDialog( null , " Error al conectarse con la base de datos"+ uni.getMessage());
+               
+           } catch (ClassNotFoundException uni) {
+               JOptionPane.showMessageDialog(null, " Error al cargar los drivers" + uni.getMessage());
+           }
+        
+       } return connection;   
+        
+        
+    }
     
-}
+ 
+ 
+    }
